e4bec8275c47c5d88ca66e3bc7f7f2a304083583
Midnight Animals <midnightanimals2359@gmail.com>
2025-10-15 18:30:35 +0800

    1015_update

---
+++ js/data-01.js
@@ -0,0 +1,67 @@
+const clothingData = {
+    "categories": [
+        {
+            "name": "顏色",
+            "type": "color",
+            "required": true,
+            "items": [
+                { "id": "color1", "name": "紅", "image": "hair1.png" },
+                { "id": "color2", "name": "橙", "image": "hair2.png" },
+                { "id": "color3", "name": "黃", "image": "hair3.png" },
+                { "id": "color4", "name": "綠", "image": "hair4.png" },
+                { "id": "color5", "name": "藍", "image": "hair5.png" },
+                { "id": "color6", "name": "靛", "image": "hair6.png" }
+            ]
+        },
+        {
+            "name": "瞳孔",
+            "type": "pupil",
+            "required": false,
+            "items": [
+                { "id": "pupil1", "name": "豎瞳", "image": "top1.png" },
+                { "id": "pupil2", "name": "圓瞳", "image": "top2.png" },
+                { "id": "pupil3", "name": "空心", "image": "top3.png" },
+                { "id": "pupil4", "name": "同心圓", "image": "top4.png" },
+                { "id": "pupil5", "name": "橫瞳", "image": "top5.png" },
+                { "id": "pupil6", "name": "鱷瞳", "image": "top6.png" }
+            ]
+        },
+        {
+            "name": "反光",
+            "type": "light",
+            "required": false,
+            "items": [
+                { "id": "light1", "name": "大圓", "image": "bottom1.png" },
+                { "id": "light2", "name": "小圓", "image": "bottom2.png" },
+                { "id": "light3", "name": "側邊", "image": "bottom3.png" },
+                { "id": "light4", "name": "空心圓", "image": "bottom4.png" },
+            ]
+        },
+        {
+            "name": "加亮顏色",
+            "type": "hightlight",
+            "required": false,
+            "items": [
+                { "id": "hightlight1", "name": "紅", "image": "shoes1.png" },
+                { "id": "hightlight2", "name": "橙", "image": "shoes2.png" },
+                { "id": "hightlight3", "name": "黃", "image": "shoes3.png" },
+                { "id": "hightlight4", "name": "綠", "image": "shoes4.png" },
+                { "id": "hightlight5", "name": "藍", "image": "shoes5.png" },
+                { "id": "hightlight6", "name": "靛", "image": "shoes6.png" }
+            ]
+        },
+        {
+            "name": "局部藏色",
+            "type": "darken",
+            "required": false,
+            "items": [
+                { "id": "darken1", "name": "紅", "image": "acc1.png" },
+                { "id": "darken2", "name": "橙", "image": "acc2.png" },
+                { "id": "darken3", "name": "黃", "image": "acc3.png" },
+                { "id": "darken4", "name": "綠", "image": "acc4.png" },
+                { "id": "darken5", "name": "藍", "image": "acc5.png" },
+                { "id": "darken6", "name": "靛", "image": "acc6.png" }
+            ]
+        }
+    ]
+};
